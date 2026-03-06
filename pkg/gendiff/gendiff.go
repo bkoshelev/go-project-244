8dbbd9b74b22ebef7033bc961c3a26e8212ff54d
bkoshelev <brskoshelev@gmail.com>
2026-03-06 20:08:41 +0300

    Шаг 4: Добавлена возможность сравнивать плоские JSON

--- pkg/gendiff/gendiff.go
+++ pkg/gendiff/gendiff.go
@@ -1,9 +1,21 @@
 package code
 
 import (
+	"errors"
 	"fmt"
+	"maps"
+	"slices"
+	"strings"
 
 	"github.com/bkoshelev/go-project-244/pkg/parsers"
+	"github.com/samber/lo"
+)
+
+const (
+	ADDED = iota
+	REMOVED
+	CHANGED
+	UNCHANGED
 )
 
 func GenDiff(filepath1, filepath2, format string) (string, error) {
@@ -19,5 +31,52 @@ func GenDiff(filepath1, filepath2, format string) (string, error) {
 		return "", fmt.Errorf("fail to parse second json file, %w", err)
 	}
 
-	return fmt.Sprintf("json1: %v, json2: %v", json1, json2), nil
+	// взято отсюда https://stackoverflow.com/a/69889828
+	json1Keys, json2Keys := slices.Collect(maps.Keys(json1)), slices.Collect(maps.Keys(json2))
+
+	keys := lo.Union(json1Keys, json2Keys)
+	slices.Sort(keys)
+
+	info := map[string]int{}
+
+	diff1, diff2 := lo.Difference(json2Keys, json1Keys)
+
+	for _, key := range keys {
+
+		switch {
+		case slices.Contains(diff1, key):
+			info[key] = ADDED
+		case slices.Contains(diff2, key):
+			info[key] = REMOVED
+		case json1[key] != json2[key]:
+			info[key] = CHANGED
+		default:
+			info[key] = UNCHANGED
+		}
+	}
+
+	var builder strings.Builder
+
+	builder.WriteString("{\n")
+
+	for _, key := range keys {
+
+		switch info[key] {
+		case UNCHANGED:
+			fmt.Fprintf(&builder, "    %s: %v\n", key, json1[key])
+		case ADDED:
+			fmt.Fprintf(&builder, "  + %s: %v\n", key, json2[key])
+		case REMOVED:
+			fmt.Fprintf(&builder, "  - %s: %v\n", key, json1[key])
+		case CHANGED:
+			fmt.Fprintf(&builder, "  - %s: %v\n", key, json1[key])
+			fmt.Fprintf(&builder, "  + %s: %v\n", key, json2[key])
+		default:
+			return "", errors.New("неизвестный тип отличия между json")
+		}
+	}
+
+	builder.WriteString("}")
+
+	return builder.String(), nil
 }
